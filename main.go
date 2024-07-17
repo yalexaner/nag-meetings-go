905c8462b919148a25003940f80b9babee8fce73
yalexaner <yalexaner@proton.me>
2024-07-17 21:30:15 +0500

    ignore missing .env file

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ var (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	calendarURL := os.Getenv("CALENDAR_URL")
