7543697a49c397176248669b3614a6aebbc6f9f0
yalexaner <yalexaner@proton.me>
2024-07-20 01:23:00 +0500

    add new messages

--- messages/messages.go
+++ messages/messages.go
@@ -1,10 +1,18 @@
 package messages
 
 const (
-	Start = "Чтобы подписаться на уведомления, нужно отправить /subscribe. Чтобы отписаться, отправь /unsubscribe"
-	Subscribed = "Подписал на получение уведомлений"
-	ErrorSubscribing = "Не удалось подписаться на уведомления"
-	Unsubscribed = "Удалил из базы на получение уведомлений"
-	ErrorUnsubscribing = "Не получилось удалить из базы на получение уведомлений"
-	UnknownCommand = "Такую команду не знаю. Доступны только /subscribe и /unsubscribe"
+	Start                     = "Я буду отправлять уведомления на STB Daily Meetings. Если ты ещё не авторизован, напиши @yalexaner.\n\nЧтобы подписаться на уведомления, нужно отправить /subscribe. Чтобы отписаться, отправь /unsubscribe"
+	Subscribed                = "Подписал на получение уведомлений"
+	ErrorSubscribing          = "Не удалось подписаться на уведомления"
+	Unsubscribed              = "Удалил из базы на получение уведомлений"
+	ErrorUnsubscribing        = "Не получилось удалить из базы на получение уведомлений"
+	NotAuthorized             = "Ты не авторизован. Напиши @yalexaner"
+	UnknownCommand            = "Такую команду не знаю. Доступны только /subscribe и /unsubscribe"
+	UnknownError              = "Произошла какая-то ошибка"
+	AllUsersAuthorized        = "Все пользователи авторизованы"
+	GetUnathorizedUsersError  = "Не удалось получить список неавторизованных пользователей"
+	AuthorizeUserQuestion     = "Авторизовать пользователя?"
+	Yes                       = "Да"
+	No                        = "Нет"
+	ChangeAuthorizationErrror = "Не удалось изменить статус авторизации пользователя"
 )
